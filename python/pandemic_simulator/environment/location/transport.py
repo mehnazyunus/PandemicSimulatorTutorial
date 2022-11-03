ac91e62d3a77b716238448cf6aff7d791d21f31b
Mehnaz Yunus <mehnaz138@gmail.com>
2022-11-03 00:02:24 -0500

    Reward function and regulations with transport

--- python/pandemic_simulator/environment/location/transport.py
+++ python/pandemic_simulator/environment/location/transport.py
@@ -1,14 +1,14 @@
 from dataclasses import dataclass
 
-from ..interfaces import BusinessLocationState, ContactRate, EssentialBusinessBaseLocation, SimTime, SimTimeTuple, LocationRule, globals, BaseLocation
+from ..interfaces import NonEssentialBusinessLocationState, ContactRate, NonEssentialBusinessBaseLocation, SimTime, SimTimeTuple, LocationRule, globals, BaseLocation
 
 __all__ = ['Transport', 'TransportState']
 
 @dataclass
-class TransportState(BusinessLocationState):
+class TransportState(NonEssentialBusinessLocationState):
     contact_rate: ContactRate = ContactRate(1, 10, 10, 0.5, 0.3, 0.3)
     visitor_time: SimTimeTuple(hours=tuple(range(7, 22)), week_days=tuple(range(0, 7)))
     open_time: SimTimeTuple = SimTimeTuple(hours=tuple(range(0, 24)), week_days=tuple(range(0, 6)))
 
-class Transport(EssentialBusinessBaseLocation[TransportState]):
+class Transport(NonEssentialBusinessBaseLocation[TransportState]):
     state_type = TransportState
